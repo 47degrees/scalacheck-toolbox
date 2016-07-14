6e9d9e5d40f4b406b019b74d8c94ebe5df0d166e
Noel Markham <noel.markham@gmail.com>
2016-07-14 13:57:30 +0100

    Granularity for Java 8

---
+++ src/main/scala/com/fortysevendeg/scalacheck/datetime/j8/granularity/package.scala
@@ -0,0 +1,35 @@
+package com.fortysevendeg.scalacheck.datetime.j8
+
+import com.fortysevendeg.scalacheck.datetime.Granularity
+import java.time.ZonedDateTime
+
+package object granularity {
+
+  implicit val seconds: Granularity[ZonedDateTime] = new Granularity[ZonedDateTime] {
+    val normalize = (dt: ZonedDateTime) => dt.withNano(0)
+    val description = "Seconds"
+  }
+
+  implicit val minutes: Granularity[ZonedDateTime] = new Granularity[ZonedDateTime] {
+    val normalize = (dt: ZonedDateTime) => dt.withNano(0).withSecond(0)
+    val description = "Minutes"
+  }
+
+  implicit val hours: Granularity[ZonedDateTime] = new Granularity[ZonedDateTime] {
+    val normalize = (dt: ZonedDateTime) => dt.withNano(0).withSecond(0).withMinute(0)
+    val description = "Hours"
+  }
+
+  implicit val days: Granularity[ZonedDateTime] = new Granularity[ZonedDateTime] {
+    val normalize = (dt: ZonedDateTime) => dt.withNano(0).withSecond(0).withMinute(0).withHour(0)
+    val description = "Days"
+  }
+
+  implicit val years: Granularity[ZonedDateTime] = new Granularity[ZonedDateTime] {
+    val normalize = (dt: ZonedDateTime) => dt.withNano(0).withSecond(0).withMinute(0).withHour(0).withDayOfYear(1)
+    val description = "Years"
+  }
+  
+
+
+}
--- src/test/scala/com/fortysevendeg/scalacheck/datetime/j8/GenJ8Properties.scala
+++ src/test/scala/com/fortysevendeg/scalacheck/datetime/j8/GenJ8Properties.scala
@@ -12,11 +12,43 @@ import com.fortysevendeg.scalacheck.datetime.instances.j8._
 
 import GenJ8._
 
+import com.fortysevendeg.scalacheck.datetime.Granularity
+
 object GenJ8Properties extends Properties("Java 8 Generators") {
 
   property("genDuration creates valid durations") = forAll(genDuration) { _ => passed }
 
-  property("genZonedDateTime created valid times") = forAll(genZonedDateTime) { _ => passed }
+  property("genZonedDateTime created valid times (with no granularity)") = forAll(genZonedDateTime) { _ => passed }
+
+  val granularitiesAndPredicates: List[(Granularity[ZonedDateTime], ZonedDateTime => Boolean)] = {
+
+    import java.time.temporal.ChronoField._
+
+    def zeroNanos(dt: ZonedDateTime)   =                    dt.get(NANO_OF_SECOND) == 0
+    def zeroSeconds(dt: ZonedDateTime) = zeroNanos(dt)   && dt.get(SECOND_OF_MINUTE) == 0
+    def zeroMinutes(dt: ZonedDateTime) = zeroSeconds(dt) && dt.get(MINUTE_OF_HOUR) == 0
+    def zeroHours(dt: ZonedDateTime)   = zeroMinutes(dt) && dt.get(HOUR_OF_DAY) == 0
+    def firstDay(dt: ZonedDateTime)    = zeroHours(dt)   && dt.get(DAY_OF_YEAR) == 1
+
+    List(
+      (granularity.seconds, zeroNanos _),
+      (granularity.minutes, zeroSeconds _),
+      (granularity.hours, zeroMinutes _),
+      (granularity.days, zeroHours _),
+      (granularity.years, firstDay _)
+    )
+  }
+
+  val granularitiesAndPredicatesWithDefault: List[(Granularity[ZonedDateTime], ZonedDateTime => Boolean)] = (Granularity.identity[ZonedDateTime], (_: ZonedDateTime) => true) :: granularitiesAndPredicates
+
+  property("genZonedDateTime with a granularity generated appropriate ZonedDateTimes") = forAll(Gen.oneOf(granularitiesAndPredicates)) { case (granularity, predicate) =>
+
+    implicit val generatedGranularity = granularity
+
+    forAll(genZonedDateTime) { dt =>
+      predicate(dt) :| s"${granularity.description}: $dt"
+    }
+  }
 
   // Guards against adding a duration to a datetime which cannot represent millis in a long, causing an exception.
   private[this] def tooLargeForAddingRanges(dateTime: ZonedDateTime, d: Duration): Boolean = {
@@ -30,22 +62,30 @@ object GenJ8Properties extends Properties("Java 8 Generators") {
     }
   }
 
-  property("genDateTimeWithinRange for Java 8 should generate ZonedDateTimes between the given date and the end of the specified Duration") = forAll(genZonedDateTime, genDuration) { (now, d) =>
+  property("genDateTimeWithinRange for Java 8 should generate ZonedDateTimes between the given date and the end of the specified Duration") = forAll(genZonedDateTime, genDuration, Gen.oneOf(granularitiesAndPredicatesWithDefault)) { case (now, d, (granularity, predicate)) =>
     !tooLargeForAddingRanges(now, d) ==> {
+
+      implicit val generatedGranularity = granularity
+
       forAll(genDateTimeWithinRange(now, d)) { generated =>
         val durationBoundary = now.plus(d)
 
         val resultText = s"""Duration:        $d
                             |Now:             $now
                             |Generated:       $generated
-                            |Period Boundary: $durationBoundary""".stripMargin
+                            |Period Boundary: $durationBoundary
+                            |Granularity      ${granularity.description}""".stripMargin
 
         val (lowerBound, upperBound) = if(durationBoundary.isAfter(now)) (now, durationBoundary) else (durationBoundary, now)
 
-        val check = (lowerBound.isBefore(generated) || lowerBound.isEqual(generated)) &&
+        val rangeCheck = (lowerBound.isBefore(generated) || lowerBound.isEqual(generated)) &&
                     (upperBound.isAfter(generated)  || upperBound.isEqual(generated))
 
-        check :| resultText
+        val granularityCheck = predicate(generated)
+
+        val prop = rangeCheck && granularityCheck
+
+        prop :| resultText
       }
     }
   }
