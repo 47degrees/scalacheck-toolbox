4c1bac684b79dee858fafa3e8a055fb2d5f168d2
Noel Markham <noel.markham@gmail.com>
2017-01-12 10:58:23 +0000

    Introduce a new combinator for picking from a map

--- combinators/src/main/scala/com/fortysevendeg/scalacheck/combinators/Combinators.scala
+++ combinators/src/main/scala/com/fortysevendeg/scalacheck/combinators/Combinators.scala
@@ -5,4 +5,21 @@
 
 package com.fortysevendeg.scalacheck.combinators
 
-object Combinators
+import org.scalacheck._
+import org.scalacheck.Gen._
+import org.scalacheck.Arbitrary.arbitrary
+
+object Combinators {
+
+  def genPickFromMapWithSuccessAndFailure[A, B](implicit arbA: Arbitrary[A], arbB: Arbitrary[B]): Gen[(Map[A, B], List[A], List[A])] = for {
+    map <- arbitrary[Map[A, B]]
+    keys = map.keySet
+    validPicks <- someOf(keys)
+    anotherList <- listOf(arbitrary[A])
+    invalidPicks = anotherList.filterNot(i => keys.contains(i))
+  } yield (map, validPicks.toList, invalidPicks)
+
+  def genPickFromMapWithSuccess[A, B](implicit arbA: Arbitrary[A], arbB: Arbitrary[B]): Gen[(Map[A, B], List[A])] =
+    genPickFromMapWithSuccessAndFailure[A, B].map { case (m, s, _) => (m, s) }
+
+}
--- combinators/src/test/scala/com/fortysevendeg/scalacheck/combinators/CombinatorProperties.scala
+++ combinators/src/test/scala/com/fortysevendeg/scalacheck/combinators/CombinatorProperties.scala
@@ -8,9 +8,16 @@ package com.fortysevendeg.scalacheck.combinators
 import org.scalacheck._
 import org.scalacheck.Prop._
 
+import Combinators._
+
 object CombinatorProperties extends Properties("Combinator Generators") {
 
-  property("this should not hold") = forAll { _ : Int =>
-    false
-  }
+  property("genPickFromMapWithSuccessAndFailure should return a list of elements in the map, and a list of elements not in the map") =
+    forAll(genPickFromMapWithSuccessAndFailure[String, String]) { case (map, succs, fails) =>
+      succs.forall(s => map.get(s).isDefined) &&
+      fails.forall(f => map.get(f).isEmpty)
+    }
+
+  property("genPickFromMapWithSuccess should return a list of elements in the map, and a list of elements not in the map") =
+    forAll(genPickFromMapWithSuccess[String, String]) { case (map, succs) => succs.forall(s => map.get(s).isDefined) }
 }
