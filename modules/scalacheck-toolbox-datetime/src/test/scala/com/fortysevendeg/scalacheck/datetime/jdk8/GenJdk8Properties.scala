b560bc7cbc981b0edea77ec1b1f7c1eea8133307
Ryan Peters <ryan.peters@47deg.com>
2021-02-03 18:33:08 -0600

    Fix typo in test

--- modules/scalacheck-toolbox-datetime/src/test/scala/com/fortysevendeg/scalacheck/datetime/jdk8/GenJdk8Properties.scala
+++ modules/scalacheck-toolbox-datetime/src/test/scala/com/fortysevendeg/scalacheck/datetime/jdk8/GenJdk8Properties.scala
@@ -30,7 +30,7 @@ import scala.util.Try
 object GenJdk8Properties extends Properties("Java 8 Generators") {
 
   // Guards against generating values well outside of expected ranges, as users may run into JDK bugs
-  implicit val yearRange = yearrange.of(0, 10000)
+  implicit val yearRange = yearrange.between(0, 10000)
 
   private val twoThousandYearsMillis = (86400000 * 2000 * 365.25).round
 
